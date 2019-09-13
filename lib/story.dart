5a908dfd4049de64c35e51a4ca6a68dd4628b436
Aakarsh Sinha <aakarshsak@gmail.com>
2019-09-13 23:14:29 +0530

    Replacing 4th todo with adding a const.

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,5 @@
 class Story {
   String storyTitle, choice1, choice2;
-}
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
